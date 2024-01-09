845ac88e2c92919467005d213975aa33c19f7cd6
Bùi Hải Triều <trieuql159@gmail.com>
2024-01-09 23:37:38 +0700

    khoc 100 lan

--- HomeStay/Areas/Admin/Controllers/AccountsController.cs
+++ HomeStay/Areas/Admin/Controllers/AccountsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HomeStay.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using X.PagedList;
 
 namespace HomeStay.Areas.Admin.Controllers
 {
