7941ab71358deec360e13e6047cdac225781e8fc
Bùi Hải Triều <trieuql159@gmail.com>
2024-03-25 15:58:24 +0700

    chan lam roi

--- HomeStay/Areas/Admin/Controllers/HomeController.cs
+++ HomeStay/Areas/Admin/Controllers/HomeController.cs
@@ -3,12 +3,24 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using HomeStay.Models;
 
 namespace HomeStay.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        [Area("Admin")]
+        private readonly HomestayDBContext _context;
+        private INotyfService _notifyService { get; }
+
+        public HomeController(HomestayDBContext context, INotyfService notifyService)
+        {
+            _context = context;
+            _notifyService = notifyService;
+
+        }
         public IActionResult Index()
         {
             var userClaims = User.Identity as ClaimsIdentity;
@@ -16,6 +28,22 @@ namespace HomeStay.Areas.Admin.Controllers
             {
                 userClaims.SetUserClaims(TempData);
             }
+
+            var LitsAccount = _context.Accounts.Count();
+            var listCustomers = _context.Customers.Count();
+            var listRooom = _context.Rooms.Count();
+            var ListBooking = _context.Bookings.Count();
+
+
+
+            var totalAmount = _context.Bookings.Sum(b => b.TotalAmount);
+
+
+            ViewData["LitsAccount"] = LitsAccount != null ? LitsAccount :  0;
+            ViewData["listCustomer"] = listCustomers != null ? listCustomers : 0;
+            ViewData["listRoom"] = listRooom != null ? listRooom : 0;
+            ViewData["ListBooking"] = ListBooking != null ? ListBooking : 0;
+            ViewData["TotalAmount"] = totalAmount;
             return View();
         }
 
